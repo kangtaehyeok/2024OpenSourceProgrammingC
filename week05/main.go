8dc8687766f30bb708b67525f1f417d76e1b5630
kangth0401 <kangth0401@naver.com>
2024-09-27 15:25:03 +0900

    v0.3 type

--- week05/main.go
+++ week05/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"strings"
 )
 
@@ -10,15 +11,18 @@ func main() {
 
 	// var i int = 55
 
-	var f float32 = 3.99 //4바이트
+	//var f float32 = 3.99 //4바이트
 	//float64 << 8바이트 double
 
 	//var i int
 	//i = 55
 
+	f := 3.99
+	//i := "55" 문자열 %s
 	i := 55
 
-	fmt.Println(f, math.Ceil(3.49))
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
+	fmt.Println(f, math.Ceil(3.49)) //floor 자리내림
 	fmt.Println(strings.Title("kim inha"))
 	fmt.Printf("i는 %d\n", i)
 	fmt.Print("i는 ", i, "\n")
