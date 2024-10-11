1405b81a48cf1cda0cb246fae5c0f3a5007d3cc1
kangth0401 <kangth0401@naver.com>
2024-10-11 14:13:06 +0900

    v1.0 Replace

--- week07/main.go
+++ week07/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-	// 오늘은 10월 11일
-	var now time.Time = time.Now()
-	fmt.Printf("%d년 %d월 %d일\n", now.Year(), now.Month(), now.Day())
-	fmt.Printf("%d년 %d월 %d일 %d시 %d분 %d초임\n", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
+	var army string = "우리는 $가와 $민에 충성을 다하는 대한민$ $이다."
+	armyFixed := strings.NewReplacer("$", "국")
+	fmt.Println(army)
+	fmt.Println(armyFixed.Replace(army))
 }
