0d308dbbe44757638288c137d5033d41fcd1731c
kangth0401 <kangth0401@naver.com>
2024-10-11 14:01:21 +0900

    v0.8 Time

--- week07/main.go
+++ week07/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
+	// 오늘은 10월 11일
 	var now time.Time = time.Now()
-	var month int = int(now.Month())
-	fmt.Println(month)
+	fmt.Printf("%d년 %d월 %d일\n", now.Year(), now.Month(), now.Day())
+	fmt.Printf("%d년 %d월 %d일 %d시 %d분 %d초임\n", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
 }
