b006b1258dde2cecedc6d1e71609943b2623973d
kangth0401 <kangth0401@naver.com>
2024-10-25 14:24:55 +0900

    v1.6 guess game

--- week07/main.go
+++ week07/main.go
@@ -11,19 +11,21 @@ import (
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
+	fmt.Print("input your name: ")
+	name, err := in.ReadString('\n')
 	fmt.Print("input your score: ")
 	i, err := in.ReadString('\n')
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	i = strings.TrimSpace(i)                //줄바꿈 등 제거 파이썬 strip 함수와 비슷
 	score, _ := strconv.ParseInt(i, 16, 32) //16진 정수형 32bit
-	if score >= 90 {
+	if score >= 60 {
 		fmt.Println("A")
 		fmt.Printf("%d\n", score)
 	} else {
+		fmt.Println(name)
 		fmt.Println("BCDF")
 		fmt.Printf("%d\n", score)
 	}
