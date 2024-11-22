fdda6a565537f35b6ca73d651bb1d666ecd5abef
kangth0401 <kangth0401@naver.com>
2024-11-22 15:20:34 +0900

    v4.4 Slice

--- week13/main.go
+++ week13/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"reflect"
 )
+
+// func test(strs string) {
+func test(strs ...string) { //func test(i int, strs ...string)
+	fmt.Println(strs, reflect.TypeOf(strs))
+}
 
 func main() {
 	//fmt.Println(os.Args[1:], len(os.Args))
@@ -14,4 +20,9 @@ func main() {
 	}
 	slices = append(slices, "forever", "!")
 	fmt.Println(slices, len(slices))
+	test("ABC")
+	test("ABC", "123")
+	test()
+	test("ABC", "123", "REAR")
+
 }
