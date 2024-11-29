dbefafd6d511a75a9c034e4477c5afaff95896f9
kangth0401 <kangth0401@naver.com>
2024-11-29 14:51:39 +0900

    v4.8 Struct

--- week14/main.go
+++ week14/main.go
@@ -2,14 +2,22 @@ package main
 
 import "fmt"
 
+type student struct {
+	id    int
+	name  string
+	grade float32
+}
+
 func main() {
-	var student struct {
-		id    int
-		name  string
-		grade float32
-	}
-	student.id = 202444077
-	student.name = "강태혁"
-	student.grade = 4.5
-	fmt.Println(student.grade)
+	var student1 student
+	student1.id = 202444077
+	student1.name = "강태혁"
+	student1.grade = 4.5
+	fmt.Println(student1.grade)
+
+	var student2 student
+	student2.id = 202444666
+	student2.name = "니얼굴"
+	student2.grade = 4.5
+	fmt.Println(student2.id)
 }
